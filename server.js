44111f042c9ec7037054f82a09799215a05f684e
gruler05 <bansodegaurav@gmail.com>
2019-03-24 20:40:46 -0500

    Fixing the production var error

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const helmet = require("helmet");
 const path = require("path");
 const users = require("./routes/api/users");
 const app = express();
 
 app.use(helmet());
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use("/api/users", users);
 
 // When Deploying it in production mode
-if (process.env.NODE_ENV === production) {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
